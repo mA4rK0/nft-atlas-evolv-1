b8686d1acc05efa02845aa959e14e0f5a970a351
marko <onlymarko25@gmail.com>
2025-02-16 22:31:12 +0700

    debug the types.

--- src/utils/nft.tsx
+++ src/utils/nft.tsx
@@ -6,9 +6,15 @@ type Nft = {
   image: {
     originalUrl: string;
   };
+  originalUrl: string;
   tokenId: string;
   tokenUri: string;
   description: string;
+  tokenType: string;
+  contract: {
+    address: string;
+  };
+  address: string;
 };
 
 const getAlchemyNetwork = (networkName: string | null): Network => {
@@ -44,9 +50,24 @@ export const fetchNFTs = async (address: string): Promise<Nft[]> => {
 
   try {
     const nfts = await alchemy.nft.getNftsForOwner(address);
-    console.log(nfts);
-    const nftList = nfts["ownedNfts"];
-    return nftList as Nft[];
+    const ownedNfts = nfts.ownedNfts;
+
+    const nftList: Nft[] = ownedNfts.map((ownedNft) => ({
+      name: ownedNft.name || "Unnamed NFT",
+      image: {
+        originalUrl: ownedNft.image?.originalUrl || "",
+      },
+      originalUrl: ownedNft.tokenUri || "",
+      tokenId: ownedNft.tokenId,
+      tokenUri: ownedNft.tokenUri || "",
+      description: ownedNft.description || "",
+      tokenType: ownedNft.contract.tokenType || "ERC721",
+      contract: {
+        address: ownedNft.contract.address,
+      },
+      address: address,
+    }));
+    return nftList;
   } catch (error) {
     console.error("Error fetching NFTs:", error);
     return [];
