19e2bf5426e57d98357ac04e4ddf7bf9191f5742
marko <onlymarko25@gmail.com>
2025-02-17 12:21:33 +0700

    adding a back button on slug page.

--- src/app/inspect/[slug]/page.tsx
+++ src/app/inspect/[slug]/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { useNftStore } from "@/global/nftStore";
 import { useParams } from "next/navigation";
+import Link from "next/link";
 
 type Nft = {
   name: string;
@@ -44,37 +45,44 @@ export default function Page() {
   }, [slug]);
 
   return (
-    <div className="w-full min-h-screen flex items-center justify-center bg-gray-900">
+    <div className="w-full min-h-screen flex flex-col gap-y-9 items-center justify-center bg-gray-900">
       {theNft && (
-        <div className="flex flex-wrap w-full max-w-7xl text-white bg-hoverNavbar p-6 sm:p-8 rounded-lg shadow-lg">
-          <div className="relative w-full sm:w-[22rem] h-[22rem] sm:h-[22rem] border bg-black rounded-xl overflow-hidden">
-            <img src={theNft.image.originalUrl} alt={theNft.name} className="absolute inset-0 w-full h-full object-cover" loading="lazy" />
-          </div>
-          <div className="flex flex-col w-full sm:w-[40rem] mt-6 sm:mt-0 sm:ml-8">
-            <div>
-              <p className="text-3xl sm:text-4xl font-medium text-white">{theNft.name}</p>
+        <>
+          <div className="flex flex-wrap w-full max-w-7xl text-white bg-hoverNavbar p-6 sm:p-8 rounded-lg shadow-lg">
+            <div className="relative w-full sm:w-[22rem] h-[22rem] sm:h-[22rem] border bg-black rounded-xl overflow-hidden">
+              <img src={theNft.image.originalUrl} alt={theNft.name} className="absolute inset-0 w-full h-full object-cover" loading="lazy" />
             </div>
-            <div className="border-4 border-white rounded-lg p-4 sm:p-5 mt-4">
-              <div className="flex justify-between items-center pb-1">
-                <p className="text-base sm:text-lg">Contract Address</p>
-                <p className="text-sm sm:text-base">{address}</p>
-              </div>
-              <div className="flex justify-between items-center pb-1">
-                <p className="text-base sm:text-lg">Token ID</p>
-                <p className="text-sm sm:text-base">{theNft.tokenId}</p>
-              </div>
-              <div className="flex justify-between items-center pb-1">
-                <p className="text-base sm:text-lg">Token Standard</p>
-                <p className="text-sm sm:text-base">{tokenType}</p>
-              </div>
+            <div className="flex flex-col w-full sm:w-[40rem] mt-6 sm:mt-0 sm:ml-8">
               <div>
-                <p className="text-base sm:text-lg pb-1">Description:</p>
-                <p className="text-sm sm:text-base">{theNft.description}</p>
+                <p className="text-3xl sm:text-4xl font-medium text-white">{theNft.name}</p>
+              </div>
+              <div className="border-4 border-white rounded-lg p-4 sm:p-5 mt-4">
+                <div className="flex justify-between items-center pb-1">
+                  <p className="text-base sm:text-lg">Contract Address</p>
+                  <p className="text-sm sm:text-base">{address}</p>
+                </div>
+                <div className="flex justify-between items-center pb-1">
+                  <p className="text-base sm:text-lg">Token ID</p>
+                  <p className="text-sm sm:text-base">{theNft.tokenId}</p>
+                </div>
+                <div className="flex justify-between items-center pb-1">
+                  <p className="text-base sm:text-lg">Token Standard</p>
+                  <p className="text-sm sm:text-base">{tokenType}</p>
+                </div>
+                <div>
+                  <p className="text-base sm:text-lg pb-1">Description:</p>
+                  <p className="text-sm sm:text-base">{theNft.description}</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </>
       )}
+      <div className="text-center text-white mb-9">
+        <Link href="/">
+          <button className="border border-hoverNavbar rounded-3xl py-2 px-28 md:px-60 transition duration-300 ease-in-out hover:shadow-md hover:shadow-hoverNavbar hover:bg-hoverNavbar">Back</button>
+        </Link>
+      </div>
     </div>
   );
 }
